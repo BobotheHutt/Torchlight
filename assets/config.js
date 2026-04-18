80977335d2fa9c182b002dff07320baae9d45025
BobotheHutt <jumprun4112@gmail.com>
2026-04-18 13:13:57 -0700

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,13 @@
+const GAME_DATA = {
+    heroes: [
+        { name: "Gemma", traits: ["Flame of Pleasure", "Frostbitten Heart", "Ice-Fire Fusion"] },
+        { name: "Rehan", traits: ["Anger", "Seething Silhouette"] },
+        { name: "Thea", traits: ["Wisdom of the Gods", "Incarnation of the Gods", "Blasphemer"] },
+        { name: "Youga", traits: ["Spacetime Illusion", "Spacetime Elapse"] },
+        { name: "Carino", traits: ["Ranger of Glory", "Lethal Flash", "Zealot of War"] }
+    ],
+    talentTrees: [
+        "God of Might", "Goddess of Hunting", "Goddess of Knowledge", 
+        "Goddess of Darkness", "God of War", "God of Machines"
+    ]
+};
